83be3cadb08d3ee89e1a0e7817f5faeea61bf8c1
Will Canfield <will.canfield14@outlook.com>
2025-08-27 16:46:35 -0600

    Cyber Daily Remap

--- patchpal/oauth.py
+++ patchpal/oauth.py
@@ -4,7 +4,7 @@ from flask import Blueprint, redirect, request, session
 from slack_sdk.oauth import AuthorizeUrlGenerator
 from slack_sdk.web import WebClient
 from .install_store import upsert_installation
-from .selector import upsert_installation  # <- use the helper you added there
+from .install_store import upsert_installation  # <- correct
 
 bp = Blueprint("slack_oauth", __name__)
 
